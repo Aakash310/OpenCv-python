49d025408d1b2dbda5f02a837ecc901dad167b09
Aakash Agarwal <54284599+Aakash310@users.noreply.github.com>
2020-10-12 00:03:44 +0530

    Add files via upload

---
+++ basic_operations_on_images.py
@@ -0,0 +1,24 @@
+import numpy as np
+import cv2
+
+img = cv2.imread("messi5.jpg")
+img2 = cv2.imread('Lenna.png')
+
+print(img.shape) #return a tuple of number if rows , columns and channel
+print(img.size)  #return total number of pixels accessed
+print(img.dtype)  #returns image datatype
+b,g,r = cv2.split(img)
+img = cv2.merge((b,g,r))
+
+ball = img[280:340,330:390]
+img[273:333,100:160] = ball
+
+img = cv2.resize(img , (512,512))
+img2 = cv2.resize(img2 , (512,512))
+
+#dst = cv2.add(img,img2)
+dst = cv2.addWeighted(img, 0.6 , img2 , 0.4 ,0)
+
+cv2.imshow("image",dst)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
